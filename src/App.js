dddfb6bb0cd6e122d17dcf8555649faf530d21d8
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-11-15 10:04:27 -0800

    added button group and button component. update to README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */ /** @jsxRuntime classic */ 
+import { jsx } from "theme-ui"
+import { useState } from 'react'
+import './App.css'
+
+const buttonGroupTheme = {
+  padding: '0px 4px',
+  height: '48px',
+  borderRadius: '4px',
+  backgroundColor: '#181A20',
+  display: 'inline-block',
+  maxWidth: 'fit-content',
+  display: 'flex',
+  alignItems: 'center'
+}
+
+const buttonTheme = {
+  height: '38px',
+  width: '130px',
+  borderRadius: '4px',
+  background: 'linear-gradient(138.01deg, #70FAF7 0%, #15D5F3 100%)',
+  cursor: 'pointer'
+}
+
+const ButtonGroup = ({items}) => {
+
+  const [buttonId, setButtonId] = useState(0)
+
+  const handleOnClick = (index) => {
+    setButtonId(index)
+  }
+
+  return (
+    <div sx={buttonGroupTheme}>
+      {
+        items.map((item, index) => {
+          return (
+              <Button onClick={() => handleOnClick(index)} index={index} active={buttonId === index ? true : false}>{item}</Button>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const Button = ({children, active, onClick, index}) => {
+  return (
+    <button onClick={() => onClick(index)} sx={buttonTheme} style={active ? {} : {background: 'none', color: '#70FAF7', border: 'none'}}>{children}</button>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center',}}>
+      <ButtonGroup items={['Cash/Debt Card', 'Manage Tokens']} />
     </div>
   );
 }
